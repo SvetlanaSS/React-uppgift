03feb6ade3ce816a7a4322991110c97cb948edfa
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-08-30 12:46:21 +0200

    nav

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+          <div><a href="#">REACT PATTERNS</a></div>
+          <div><a href="#">REACT COMPOSITION</a></div>
+    </div>
+  );
+}
+
+export default Nav;
